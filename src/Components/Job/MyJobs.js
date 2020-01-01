d4020af270e93a0a0e858191e72ada96d3dc0571
Arshaq <arshaqshakeel3@gmail.com>
2020-01-01 20:41:05 +0500

    get ny jobs is pending

---
+++ src/Components/Job/MyJobs.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import React, { Component } from "react";
+import Navbar from "../Navbar/Navbar";
+
+export default class Myjobs extends Component {
+    
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <h2>My jObs</h2>
+      </div>
+    );
+  }
+}
